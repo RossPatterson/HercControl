c2b4275ef917d66c691a8670c7c68062891bd24d
Ross Patterson <ross.patterson@gmail.com>
2025-11-02 18:11:33 -0500

    Get 200 console lines

--- src/HercControl.cpp
+++ src/HercControl.cpp
@@ -375,7 +375,7 @@ void callHerculesConsole(string command, int requested_console_size, vector<stri
 		cerr << rang::fg::cyan << "CHC2: Command: " << cmd << rang::style::reset << endl;
 	while (secondsSince(begin) < timeOut) {
 
-		http_call = cpr::Get(cpr::Url{ cmd }, cpr::Parameters{ {"command"s, command}, {"msgcount"s, /*to_string(lines_wanted)*/ "0"} });
+		http_call = cpr::Get(cpr::Url{ cmd }, cpr::Parameters{ {"command"s, command}, {"msgcount"s, /*to_string(lines_wanted)*/ "200"} });
 		if (debug) {
 			cerr << rang::fg::cyan << "CHC2: Error Code: "s << (int)http_call.error.code << rang::style::reset << endl;
 			cerr << rang::fg::cyan << "CHC2: Error Message: "s << http_call.error.message << rang::style::reset << endl;
